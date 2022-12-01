ca95a9e55f1661c1752f023646e8eb897c4a60fe
Lucas <contato@lucascesar.com.br>
2022-12-01 19:27:46 -0300

    project setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { checkFileUpdate } from './fileChecker.js';
+
+const BARONY_SAVEFILES_PATH = 'C:/Program Files/Epic Games/BaronyBeta/savegames';
+const BARONY_SAVEGAME_FILE = 'savegame2_mp.baronysave';
+
+let lastFileUpdate = "";
+
+setInterval(() => {
+    console.log('')
+
+}, 1000);
---
+++ src/fileChecker.js
@@ -0,0 +1,3 @@
+export function checkFileUpdate(path, lastUpdate) {
+    
+}
