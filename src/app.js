de94e2e43ce67159fa0e07969428cb34cccbc3a0
Lucas <contato@lucascesar.com.br>
2022-12-01 21:51:10 -0300

    feat: added file watcher and created backup file function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import { checkFileUpdate } from './fileChecker.js';
+import fs from 'fs';
+import path from 'path';
 
-const BARONY_SAVEFILES_PATH = 'C:/Program Files/Epic Games/BaronyBeta/savegames';
-const BARONY_SAVEGAME_FILE = 'savegame2_mp.baronysave';
+import config from './config/config.js';
 
-let lastFileUpdate = "";
+const BARONY_BACKUPS_DIRECTORY = config.backups_directory_path;
+const BARONY_SAVEFILES_PATH = config.savefiles_path;
+const BARONY_SAVEGAME_FILE = config.savegame_file;
 
-setInterval(() => {
-    console.log('')
+const saveFilePath = path.join(BARONY_SAVEFILES_PATH, BARONY_SAVEGAME_FILE);
 
-}, 1000);
+fs.watchFile(saveFilePath, { interval: 2000 }, () => {
+    createBackupFile();
+});
+
+function createBackupFile() {
+    const backupsFolderPath = path.join(BARONY_BACKUPS_DIRECTORY, 'Barony Backups');
+
+    if (!fs.existsSync(backupsFolderPath)) {
+        fs.mkdirSync(backupsFolderPath);
+    }
+
+    const timestamp = Date.now().toString();
+    const backupFolderPath = path.join(backupsFolderPath, timestamp);
+    
+    fs.mkdirSync(path.join(backupFolderPath))
+
+    const backupFilePath = path.join(backupFolderPath, BARONY_SAVEGAME_FILE)
+
+    fs.copyFileSync(saveFilePath, backupFilePath);
+
+    const currentDate = new Date();
+
+    console.log(`[${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}] Backup feito com sucesso`);
+}
---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+export default {
+    "savefiles_path": "C:\\Program Files\\Epic Games\\BaronyBeta\\savegames",
+    "savegame_file": "savegame0.baronysave",
+    "backups_directory_path": "C:\\Users\\Lucas\\Documents"
+}
